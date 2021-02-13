1e61b7a43f76c834a0491a989613f18313fb897f
aleksandartepavcevic <atepavcevic29@gmail.com>
2021-02-14 00:46:54 +0100

    imgTask added

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
+const imagemin = require('gulp-imagemin');
 
 function copyHtml () {
     return gulp.src('./src/*.html').pipe(gulp.dest('./dist'));
@@ -19,9 +20,22 @@ function watch () {
             baseDir: './dist'
         }
     });
+}
 
-    gulp.watch('./src/scss/**/*.scss', style);
-    gulp.watch('./src/*.html', copyHtml).on('change', browserSync.reload);
+function imgTask () {
+    return gulp.src('./src/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+}
+
+function start () {
+    copyHtml();
+    style();
+    watch();
+    imgTask();
+
+    gulp.watch('./src/scss/**/*.scss').on('change', style);
+    gulp.watch('./src/*.html').on('change', copyHtml).on('change', browserSync.reload);
     gulp.watch('./src/js/**/*.js').on('change', browserSync.reload);
 }
 
@@ -30,3 +44,5 @@ function watch () {
 exports.style = style;
 exports.watch = watch;
 exports.copyHtml = copyHtml;
+exports.start = start;
+exports.imgTask = imgTask;
